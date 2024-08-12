98ac9c71a8e7840d5b740eb44b20388198116ce7
Blessing <ogorblessing96@gmail.com>
2024-08-12 04:51:25 +0000

    Task 2 done

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,10 @@
+$('document').ready(function(){
+  let amenities = {}
+  $('input[type="checkbox"]').change(function(){
+    if amenities[$(this).attr('data_id')] = $(this).attr('data_name');
+    ekse {
+      delete amenities[$(this).attr('data_id')];
+    }
+    }
+  })
+})
